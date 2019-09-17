fe4d0a1377b1eb634b53e48021e48425505ab204
Cameron <cameronjmaccabe@gmail.com>
2019-09-17 06:49:18 -0700

    added sorting functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,11 @@ const bands = [
   "Anywhere but Here",
   "An Old Dog"
 ];
+
+function strip(name) {
+  return name.replace(/^(the |a |an )/i, "").trim();
+}
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector("#bands").innerHTML = sortedBands.map(name => `<li>${name}</li>`).join("");
